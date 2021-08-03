214b3bea32a27c5b2408de2a414a14541767a200
MicaioValente <85925562+MicaioValente@users.noreply.github.com>
2021-08-03 16:19:47 -0300

    Add files via upload

---
+++ styles/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle, } from 'styled-components';
+const GlobalStyle = createGlobalStyle` 
+    body {
+    margin: 0;
+    padding: 1px;
+    box-sizing: border-box;
+    background-color: #4a4a4a;
+  } `
+
+
+export default GlobalStyle
---
+++ styles/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+    colors: {
+      primary: '#00ff51',
+    },
+  }
+
+  export default theme
